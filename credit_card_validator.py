5712ccd02c02a7a62f99f5836a2b178950d039a6
Jheave <jimenezjheave123@gmail.com>
2021-11-02 19:44:31 +0800

    assiginment in game design

---
+++ credit_card_validator.py
@@ -0,0 +1,70 @@
+"""
+
+ Implemented using  Luhn’s algorithm
+ https://www.geeksforgeeks.org/luhn-algorithm/
+
+"""
+
+
+def main():
+    # asking the user to enter
+    credit_card_number = int(input("Please enter credit card number: "))
+
+    # while credit_card_number < 0:
+    if is_valid(credit_card_number):
+        credit_card_brand(credit_card_number)
+    else:
+        print("INVALID")
+
+
+# check if it is valid
+def is_valid(credit_card_number):
+    length = find_length(credit_card_number)
+    if (length == 13 or length == 15 or length == 16) and check_sum(credit_card_number):
+        return 0
+    else:
+        return 1
+
+
+# find the length of credit card
+def find_length(credit_card_number):
+    count = 0
+    while credit_card_number > 0:
+        credit_card_number = credit_card_number // 10
+        count = count + 1
+
+    return count
+
+
+# checking if credit card is == 0
+def check_sum(number):
+    credit_card_sum = 0
+    count = 0
+    while number > 0:
+        number = number // 10
+        count = count + 1
+        if count % 2 == 0:
+            credit_card_sum += number % 10
+        else:
+            digit = 2 * (number % 10)
+            credit_card_sum += digit / 10 + digit % 10
+
+    return (credit_card_sum % 10) == 0
+
+
+# for printing credit card brand   
+
+def credit_card_brand(number):
+    # checking 34 is the first 2 digit and 'e13' = 13 zero
+    if 34e13 <= number < 35e13 or 37e13 <= number < 38e13:
+        print("AMEX")
+    elif 51e14 <= number < 56e14:
+        print("MASTERCARD")
+    elif 4e12 <= number < 5e12 or 4e15 <= number < 5e15:
+        print("VISA")
+    else:
+        print("INVALID")
+
+
+if __name__ == '__main__':
+    main()
---
+++ what_grade_are_you.py
@@ -0,0 +1,43 @@
+"""
+  Implemented using Coleman - Liau formula
+  https://en.wikipedia.org/wiki/Coleman%E2%80%93Liau_index
+
+"""
+
+
+# Declare a variables
+# find a word and count that
+# find all sentence that have '.' '!' '?'
+def main():
+    letter = 0
+    word = 0
+    sentence = 0
+
+    # ask the user to enter a text
+    string = input(str("Text: "))
+
+    for elem in range(0, len(string)):
+        if str.isalpha(string[elem]):
+            letter += 1
+        if elem == 0 and string[elem] != ' ' or elem != len(string) - 1 and \
+                string[elem] == ' ' and string[elem + 1] != '':
+            word += 1
+        if string[elem] == '.' or string[elem] == '?' or string[elem] == '!':
+            sentence += 1
+
+    word = float(word)  # convert int to float
+
+    L = (letter / word) * 100  # average the number of letter per 100 words
+    S = (sentence / word) * 100  # average number of sentence per 100 words
+    index = round(0.0588 * L - 0.296 * S - 15.8)
+
+    if index < 1:
+        print("Before Grade 1")
+    elif index >= 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {index}")
+
+
+if __name__ == '__main__':
+    main()
